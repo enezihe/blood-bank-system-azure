74f4960cc96becdbab6aa2e21dac025f3204f1de
enezihe <enezihe@gmail.com>
2025-07-06 11:14:06 -0400

    Add index.php for default redirect

--- admin/conn.php
+++ admin/conn.php
@@ -1,12 +1,24 @@
 <?php
-$server = "blooddbserver.mysql.database.azure.com";      
-$username = "bloodadmin@blooddbserver";                  
-$password = "Admin123";                       
-$database = "blood_donation";                      
+$server = "blooddbserver.mysql.database.azure.com";
+$username = "bloodadmin@blooddbserver";
+$password = "Admin123";
+$database = "blood_donation";
 
-$conn = mysqli_connect($server, $username, $password, $database);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+mysqli_real_connect(
+    $conn,
+    $server,
+    $username,
+    $password,
+    $database,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (mysqli_connect_errno($conn)) {
+    die("❌ Connection failed: " . mysqli_connect_error());
 }
 ?>
--- ticker.php
+++ ticker.php
@@ -1,61 +1,66 @@
-
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<style>
-@media screen and (max-width: 80px) {
-  .news {
-    position:relative;margin-left:auto;margin-right:auto;margin-top:400px  ;
-  }
-  .text1{
-  	box-shadow:none !important;
-    position:relative;
-      margin-left:auto;margin-right:auto;
-  }
-}
-.blue {
-    background: #347fd0;
-}
+  <meta charset="UTF-8">
+  <title>Blood Bank Ticker</title>
+  <style>
+    @media screen and (max-width: 80px) {
+      .news {
+        position: relative;
+        margin: 400px auto 0;
+      }
 
-.news {
-    box-shadow: inset 0 -15px 30px rgba(10,4,60,0.4), 0 5px 10px rgba(10,20,100,0.5);
-       width: 100%;
-       height:50px;
-    margin-top:0px;
-    overflow: hidden;
+      .text1 {
+        box-shadow: none !important;
+        position: relative;
+        margin: 0 auto;
+      }
+    }
 
-    border-radius: 4px;
-    padding: 1px;
-    -webkit-user-select: none;
+    .blue {
+      background: #347fd0;
+    }
 
-}
+    .news {
+      box-shadow: inset 0 -15px 30px rgba(10, 4, 60, 0.4), 0 5px 10px rgba(10, 20, 100, 0.5);
+      width: 100%;
+      height: 50px;
+      margin-top: 0;
+      overflow: hidden;
+      border-radius: 4px;
+      padding: 1px;
+      user-select: none;
+    }
 
+    .news span {
+      float: left;
+      color: #fff;
+      padding: 9px;
+      position: relative;
+      top: 1%;
+      box-shadow: inset 0 -15px 30px rgba(0, 0, 0, 0.4);
+      font: 16px 'Raleway', Helvetica, Arial, sans-serif;
+      cursor: pointer;
+    }
 
-.news span {
-    float: left;
-    color: #fff;
-    padding: 9px;
-    position: relative;
-    top: 1%;
-    box-shadow: inset 0 -15px 30px rgba(0,0,0,0.4);
-    font: 16px 'Raleway', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -webkit-user-select: none;
-    cursor: pointer;
-}
-
-.text1{
-
-	box-shadow:none !important;
-  position:relative;
-    width:90%
-}
-</style>
+    .text1 {
+      box-shadow: none !important;
+      position: relative;
+      width: 90%;
+    }
+  </style>
 </head>
 <body>
-<div class="news blue">
-<span style="background-color:#F60F0F;width:125px;height:50px">Latest Updates</span><span class="text1" ><marquee>First blood donation camp to be organised in Hisar By Varun in collabration with Blood Bank& Donation Management System on <b>01/09/2020 at Community Centre PLA.</b> Come and Be a Part of this Noble Cause :) </marquee></span>
-</div>
+
+  <div class="news blue">
+    <span style="background-color:#F60F0F; width:125px; height:50px;">Latest Updates</span>
+    <span class="text1">
+      <marquee>
+        First blood donation camp to be organised in Hisar by Varun in collaboration with Blood Bank & Donor Management System on
+        <b>01/09/2020 at Community Centre PLA.</b> Come and be a part of this noble cause :)
+      </marquee>
+    </span>
+  </div>
+
 </body>
-</head>
 </html>
